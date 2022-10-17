f07ce174a7eb32b815baed6f2a4d8ef7447984d7
Piyushnmj <piyushnmj@yahoo.com>
2022-10-18 00:00:23 +0530

    Flip Coin and print percentage of Heads and Tails

--- BasicCoreProblems/BasicCoreProblems/Program.cs
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -1,8 +1,25 @@
-﻿class Program
+﻿class FlipCoin
 {
-    static void Main(string[] args)
+    static void Main(String[] args)
     {
-        Console.WriteLine("Welcome!");
+        double heads = 0;
+        int coinFlipLoop = 1;
+        
+        while (coinFlipLoop <= 10000)
+        {
+            Random random = new Random();
+            int coinFlip = random.Next(0, 2);
+
+            if (coinFlip <= .5)
+                heads++;
+
+            coinFlipLoop = coinFlipLoop + 1;
+        }
+        double percentHeads = (heads / 10000) * 100;
+        double percentTails = ((10000 - heads) / 10000) * 100;
+
+        Console.WriteLine("The percentage of heads is: " + percentHeads);
+        Console.WriteLine("The percentage of tails is: " + percentTails);
         Console.ReadLine();
     }
 }
