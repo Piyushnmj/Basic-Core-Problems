a0c842c6c2915bea79d39b8021f617dd8838d31e
Piyushnmj <piyushnmj@yahoo.com>
2022-10-18 12:04:08 +0530

    The input year is a Leap Year or not

--- BasicCoreProblems/BasicCoreProblems/Program.cs
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -1,32 +1,18 @@
-﻿class FlipCoin
+﻿class LeapYear
 {
     static void Main(String[] args)
     {
-        double tails = 0;
-        double heads = 0;
-        int loopCount = 1;
+        Console.WriteLine("Enter the Year in 4 digits: ");
+        int Year = Convert.ToInt32(Console.ReadLine());
 
-        while (loopCount <= 10000)
+        if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
         {
-            Random random = new Random();
-            double coinFlip = random.Next(0, 2);
-
-            if (coinFlip < 0.5)
-            {
-                tails++;
-            }
-            else
-            {
-                heads++;
-            }
-
-            loopCount = loopCount + 1;
+            Console.WriteLine("{0} is a Leap Year", Year);
         }
-        double percentTails = (tails / loopCount) * 100;
-        double percentHeads = (heads / loopCount) * 100;
-
-        Console.WriteLine("The percentage of heads is: " + percentHeads + " % ");
-        Console.WriteLine("The percentage of tails is: " + percentTails + " % ");
+        else
+        {
+            Console.WriteLine("{0} is not a Leap Year", Year);
+        }
         Console.ReadLine();
     }
 }
