1f6a23ad6fe50c79efe72f074daf2ca93a40c932
Piyushnmj <piyushnmj@yahoo.com>
2022-10-18 23:17:14 +0530

    Print Table of the Powers of 2

--- BasicCoreProblems/BasicCoreProblems/Program.cs
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-﻿class Swap2Num
-{
-    static void Main(String[] args)
-    {
-        Console.WriteLine("Enter a value: ");
-        int N = Convert.ToInt32(Console.ReadLine());
-        int i = 0;
-
-        for (i = 1; i <= N; i++)
-        {
-            if ( N % i == 0)
-            {
-                Console.WriteLine(i + " is a Factor of " + N);
-            }
-            else
-            {
-                Console.WriteLine(i + " is not a Factor of " + N);
-            }
-=======
-﻿class Powerof2
+class Powerof2
 {
     static void Main(String[] args)
     {
@@ -29,8 +9,7 @@
         {
             Console.WriteLine(num);
             num = num * 2;
->>>>>>> PowerOf2
         }
-        Console.ReadLine();
+        
     }
 }
