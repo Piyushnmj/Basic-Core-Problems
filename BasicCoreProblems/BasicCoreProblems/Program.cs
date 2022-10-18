fff5218124280acf02f5a94400efe1ce1d0a19c2
Piyushnmj <piyushnmj@yahoo.com>
2022-10-18 21:22:38 +0530

    Swap Two Numbers

--- BasicCoreProblems/BasicCoreProblems/Program.cs
+++ BasicCoreProblems/BasicCoreProblems/Program.cs
@@ -1,27 +1,20 @@
-﻿class QuotientAndRemainder
+﻿class Swap2Num
 {
     static void Main(String[] args)
     {
-<<<<<<< HEAD
-        Console.WriteLine("Enter a Dividend: ");
-        int dividend = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter a Divisor: ");
-        int divisor = Convert.ToInt32(Console.ReadLine());
-=======
         Console.WriteLine("Enter value of a: ");
         int a = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter value of b: ");
         int b = Convert.ToInt32(Console.ReadLine());
         int temp;
-        
+
         Console.WriteLine("Before Swapping: Value of a={0}, b={1}", a, b);
->>>>>>> Swap2Num
 
-        int quotient = dividend / divisor;
-        int remainder = dividend % divisor;
+        temp = a;
+        a = b;
+        b = temp;
 
-        Console.WriteLine("Quotient = " + quotient);
-        Console.WriteLine("Remainder = " + remainder);
+        Console.WriteLine("After Swapping: Value of a={0}, b={1}", a, b);
         Console.ReadLine();
     }
 }
